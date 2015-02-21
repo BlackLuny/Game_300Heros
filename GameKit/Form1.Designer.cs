32706ffa7f10b9fc15e57f595470411e544922bb
number201724 <number201724@me.com>
2015-02-21 17:58:04 +0800

    x

---
+++ GameKit/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace GameKit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new ListViewNF();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.复制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(2, 2);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(781, 495);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IsSend";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Bytes";
+            this.columnHeader2.Width = 700;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(799, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "清空";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(811, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Send";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(811, 77);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Recv";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.复制ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 复制ToolStripMenuItem
+            // 
+            this.复制ToolStripMenuItem.Name = "复制ToolStripMenuItem";
+            this.复制ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.复制ToolStripMenuItem.Text = "复制";
+            this.复制ToolStripMenuItem.Click += new System.EventHandler(this.复制ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 514);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(606, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(705, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "recv";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 546);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListViewNF listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 复制ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ GameKit/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GameKit
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern long SendMessage(Int32 hwnd, Int32 msg, Int32 hwndFrom, ref COPYDATASTRUCT cd);
+
+        [DllImport("user32.dll")]
+        private static extern Int32 FindWindow(String classname, String text);
+        
+        const int WM_COPYDATA = 0x004A;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COPYDATASTRUCT
+        {
+            public uint dwData;
+            public int cbData;
+            public IntPtr lpData;
+        }
+
+        public class GameMessageData
+        {
+            public byte[] buffer;
+            public uint type;
+        }
+
+        private Queue<GameMessageData> MessageDataQueue = new Queue<GameMessageData>();
+
+        public static string BytesToString(byte[] sb)
+        {
+            string temp = "";
+            foreach (byte b in sb)
+            {
+                temp += String.Format("{0:X2} ", b);
+            }
+            return temp;
+        }
+
+        private static byte[] HexToByte(string hexString)
+        {
+            string newstr = hexString.Replace(" ", "");
+            byte[] returnBytes = new byte[newstr.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(newstr.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Title = "GameKit";
+            this.Text = "GameKit_Protocol";
+
+
+            //listView1.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        private void AddPackets(GameMessageData Data)
+        {
+            if (Data.type == 0)
+            {
+                if (checkBox2.Checked)
+                {
+                    ListViewItem lv = listView1.Items.Add(Data.type.ToString());
+                    lv.SubItems.Add(BytesToString(Data.buffer));
+                }
+            }
+            else
+            {
+                if (checkBox1.Checked)
+                {
+                    ListViewItem lv = listView1.Items.Add(Data.type.ToString());
+                    lv.SubItems.Add(BytesToString(Data.buffer));
+                }
+            }
+           
+        }
+
+
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_COPYDATA)
+            {
+                COPYDATASTRUCT cds = new COPYDATASTRUCT();
+                Type mytype = cds.GetType();
+                cds = (COPYDATASTRUCT)m.GetLParam(mytype);//当前的消息
+
+                byte[] bufs = new byte[cds.cbData];//注意，将结构体的成员单个处理再使用
+                Marshal.Copy(cds.lpData, bufs, 0, bufs.Length);//复制消息
+
+                GameMessageData MessageData = new GameMessageData();
+                MessageData.buffer = bufs;
+                MessageData.type = cds.dwData;
+
+                MessageDataQueue.Enqueue(MessageData);
+
+                //AddPackets(MessageData);
+            }
+            base.DefWndProc(ref m);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //listView1.BeginUpdate();
+            while (MessageDataQueue.Count > 0)
+            {
+                AddPackets(MessageDataQueue.Dequeue());
+            }
+            //listView1.EndUpdate();
+        }
+
+        private void 复制ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 0)
+            {
+                ListViewItem lv = listView1.SelectedItems[0];
+                Clipboard.SetDataObject(lv.SubItems[1].Text, true);
+            }
+            //Clipboard.SetFileDropList
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = HexToByte(textBox1.Text);
+
+                COPYDATASTRUCT cds = new COPYDATASTRUCT();
+
+                cds.dwData = (uint)1;
+
+                cds.cbData = data.Length;
+
+                cds.lpData = Marshal.AllocHGlobal(data.Length);
+
+                Marshal.Copy(data, 0, cds.lpData, data.Length);
+
+                int hWnd = FindWindow("WWW_JUMPW_COM", null);
+
+                if (hWnd != 0)
+                {
+                    SendMessage(hWnd, WM_COPYDATA, 0, ref cds);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] data = HexToByte(textBox1.Text);
+
+                COPYDATASTRUCT cds = new COPYDATASTRUCT();
+
+                cds.dwData = (uint)0;
+
+                cds.cbData = data.Length;
+
+                cds.lpData = Marshal.AllocHGlobal(data.Length);
+
+                Marshal.Copy(data, 0, cds.lpData, data.Length);
+
+                int hWnd = FindWindow("WWW_JUMPW_COM", null);
+
+                if (hWnd != 0)
+                {
+                    SendMessage(hWnd, WM_COPYDATA, 0, ref cds);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
