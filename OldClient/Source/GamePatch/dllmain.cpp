d35b5764e2839bcf7449603148db1034360fe403
number201724 <number201724@me.com>
2016-06-08 14:07:49 +0800

    xx

--- OldClient/Source/GamePatch/dllmain.cpp
+++ OldClient/Source/GamePatch/dllmain.cpp
@@ -5,9 +5,54 @@
 #include <stdlib.h>
 #include <set>
 #include <string>
-
+#include "md5_c.h"
+#include "xorstr.h"
 
 #pragma pack(1)
+
+int md5file( char *real_num2, char *file ) 
+{
+	FILE* fp;
+	long f_size;
+	int numread;
+	int i;
+
+	char r_num[33]="";
+	char *buffer = NULL;
+	unsigned char binary[ 17 ];
+	struct MD5Context context;
+
+	fp = fopen( file, "rb" );
+
+	if ( fp == NULL ) {
+		return ( -1 );
+	}
+
+	MD5Init( &context );
+
+	fseek(fp,0L,SEEK_END);
+	f_size = ftell(fp);
+	buffer = (char*)malloc(f_size + 1);
+	fseek(fp,0L,SEEK_SET);
+
+	while ( ( numread = fread(buffer,1,sizeof(buffer),fp) ) > 0 ) {
+		MD5Update( &context, (const unsigned char*)buffer, numread );
+	}
+
+	MD5Final( binary, &context );
+	free( buffer );
+
+	for ( i = 0; i < 16; i++ ) {
+		sprintf( r_num, "%s%02X", r_num,binary[ i ] );
+	}
+
+	strcpy(real_num2,r_num);
+
+	fclose(fp);
+	fp=NULL;
+
+	return ( 0 );
+}
 
 struct file_node_s
 {
@@ -82,6 +127,7 @@ DWORD GetPlayerRole() {
 	__asm mov result, eax;
 	return result;
 }
+
 DWORD GetEncodePtr(DWORD valueA, DWORD valueB)
 {
 	DWORD result = 0;
@@ -249,7 +295,6 @@ DWORD WINAPI UpdateThread(LPVOID n)
 VOID CALLBACK timerProc(HWND, UINT, UINT_PTR, DWORD)
 {
 	HotKey.Update();
-
 }
 
 void CloseDiedWindow()
@@ -259,6 +304,7 @@ void CloseDiedWindow()
 
 DWORD WINAPI FuckWindowThread(VOID*)
 {
+	MessageBoxA(NULL,/*http://bbs.300yx.net/*/XorStr<0x5D,22,0x95902684>("\x35\x2A\x2B\x10\x5B\x4D\x4C\x06\x07\x15\x49\x5B\x59\x5A\x12\x14\x43\x00\x0A\x04\x5E"+0x95902684).s,/*300英雄客户端-黑瞳版*/XorStr<0xC6,21,0x270C707D>("\xF5\xF7\xF8\x1A\x68\x1B\x17\x72\x03\x74\x77\x67\x19\xFE\x6E\x0F\x1B\x7C\x68\x3F"+0x270C707D).s,MB_OK);
 	return 0;
 }
 
@@ -275,7 +321,7 @@ void LoadDataFile()
 	void* source_buf;
 	unsigned int source_out_len;
 
-	fd = fopen("data.jmp", "rb");
+	fd = fopen(/*data.jmp*/XorStr<0x87,9,0x0065F57C>("\xE3\xE9\xFD\xEB\xA5\xE6\xE0\xFE"+0x0065F57C).s, /*rb*/XorStr<0xF7,3,0x44579C01>("\x85\x9A"+0x44579C01).s);
 	if (fd) {
 		fread(&header, sizeof(struct file_header_s), 1, fd);
 
@@ -289,7 +335,7 @@ void LoadDataFile()
 		fclose(fd);
 	}
 	else {
-		MessageBoxA(NULL, "打开Data.jmp失败", "http://www.300yx.net/", MB_ICONERROR);
+		MessageBoxA(NULL, /*打开Data.jmp失败*/XorStr<0xFF,17,0xFE200A49>("\x4B\xF2\xBE\xA8\x47\x65\x71\x67\x29\x62\x64\x7A\xC1\xAB\xBD\xD2"+0xFE200A49).s, /*http://www.300yx.net/*/XorStr<0xC5,22,0x187E1C01>("\xAD\xB2\xB3\xB8\xF3\xE5\xE4\xBB\xBA\xB9\xE1\xE3\xE1\xE2\xAA\xAC\xFB\xB8\xB2\xAC\xF6"+0x187E1C01).s, MB_ICONERROR);
 		exit(0);
 	}
 
@@ -328,7 +374,7 @@ bool IsSkinExHero(unsigned char* pHero)
 {
 	char szSkinSkillPath[MAX_PATH];
 	char hinfo[128];
-	sprintf(hinfo,"%d_%d",GetHeroID(pHero),GetSkinID(pHero));
+	sprintf(hinfo,/*%d_%d*/XorStr<0x5E,6,0x7F5F29FE>("\x7B\x3B\x3F\x44\x06"+0x7F5F29FE).s,GetHeroID(pHero),GetSkinID(pHero));
 	if(hero_info.find(hinfo) != hero_info.end())
 	{
 		return true;
@@ -338,15 +384,15 @@ bool IsSkinExHero(unsigned char* pHero)
 	{
 		strcpy(szSkinSkillPath, g_pszPathName);
 		strlwr(szSkinSkillPath);
-		if(strstr(szSkinSkillPath,"ui\\icon\\skill") != NULL)
+		if(strstr(szSkinSkillPath,/*ui\\icon\\skill*/XorStr<0x25,14,0xEE67302C>("\x50\x4F\x7B\x41\x4A\x45\x45\x70\x5E\x45\x46\x5C\x5D"+0xEE67302C).s) != NULL)
 		{
-			char* p = strstr(szSkinSkillPath, "..");
+			char* p = strstr(szSkinSkillPath, /*..dds*/XorStr<0xA0,6,0x52DF7F3B>("\x8E\x8F\xC6\xC7\xD7"+0x52DF7F3B).s);
 			if (p) {
-				sprintf(p, "._skin%d.dds", GetSkinID(pHero));
+				sprintf(p, /*._skin%d.dds*/XorStr<0x5F,13,0xBC73D744>("\x71\x3F\x12\x09\x0A\x0A\x40\x02\x49\x0C\x0D\x19"+0xBC73D744).s, GetSkinID(pHero));
 
 				strlwr(szSkinSkillPath);
 
-				std::string file = "..";
+				std::string file = /*..*/XorStr<0xDF,3,0x5FCE23E9>("\xF1\xCE"+0x5FCE23E9).s;
 				file += szSkinSkillPath;
 				if (g_FileSet.find(file) != g_FileSet.end())
 				{
@@ -358,12 +404,7 @@ bool IsSkinExHero(unsigned char* pHero)
 				}
 			}
 		}
-		
-
-		
 	}
-
-
 
 	return g_pIsSkinExHero(pHero);
 }
@@ -398,6 +439,8 @@ void *g_pGetSkillDesc = (void *)0x00807BE0;
 std::set<int> g_vHasSkillSkins;
 unsigned int (*get_skill_class)() = (unsigned int (__cdecl *)(void))0x008223A0;
 
+std::set<int> g_vIsChecked;
+
 unsigned char* __stdcall GetSkillDesc(int SkillId)
 {
 	unsigned char* result;
@@ -428,22 +471,28 @@ unsigned char* __stdcall GetSkillDesc(int SkillId)
 		mov result,eax;
 	}
 
-	if(g_vHasSkillSkins.find(SkillId) == g_vHasSkillSkins.end())
+	if(g_vHasSkillSkins.find(SkillId) != g_vHasSkillSkins.end())
 	{
-		for(int i=1;i<10;i++)
-		{
-			sprintf(uipath,"..\\ui\\icon\\skill\\ico_%d._skin%d.dds",SkillId,i);
-			if (g_FileSet.find(uipath) != g_FileSet.end())
-			{
-				g_vHasSkillSkins.insert(SkillId);
-				modify_ico = true;
-				break;
-			}
-		}
+		modify_ico = true;
 	}
 	else
 	{
-		modify_ico=true;
+		if(g_vIsChecked.find(SkillId) == g_vIsChecked.end())
+		{
+			for(int i=1;i<5;i++)
+			{
+				sprintf(uipath,/*..\\ui\\icon\\skill\\ico_%d._skin%d.dds*/XorStr<0x37,36,0x9FDE72B3>("\x19\x16\x65\x4F\x52\x60\x54\x5D\x50\x2E\x1D\x31\x28\x2D\x29\x2A\x1B\x21\x2A\x25\x14\x69\x29\x60\x10\x23\x3A\x3B\x3D\x71\x31\x78\x33\x3C\x2A"+0x9FDE72B3).s,SkillId,i);
+				if (g_FileSet.find(uipath) != g_FileSet.end())
+				{
+					g_vHasSkillSkins.insert(SkillId);
+					modify_ico = true;
+					break;
+				}
+			}
+
+			g_vIsChecked.insert(SkillId);
+		}
+		
 	}
 
 
@@ -453,15 +502,22 @@ unsigned char* __stdcall GetSkillDesc(int SkillId)
 		*(WORD*)&result[0x14] = 0x2;
 	}
 
-
-
-
-
 	return result;
 }
 
 void Initialize()
 {
+	char md5[64] = {0};
+	char filename[MAX_PATH];
+	GetModuleFileNameA(NULL,filename,sizeof(filename));
+	md5file(md5,filename);
+
+	if(strcmp(md5,/*00FA52F1ECA84DCBE4D3D8EBFA8FA4CA*/XorStr<0x7F,33,0xA75CA3CD>("\x4F\xB0\xC7\xC3\xB6\xB6\xC3\xB7\xC2\xCB\xC8\xB2\xBF\xC8\xCE\xCC\xCA\xA4\xD5\xA1\xD7\xAC\xD0\xD4\xD1\xD9\xA1\xDC\xDA\xA8\xDE\xDF"+0xA75CA3CD).s) != 0)
+	{
+		MessageBoxA(NULL,/*300Hero.exe文件不正确*/XorStr<0xBD,22,0x45D61C0D>("\x8E\x8E\x8F\x88\xA4\xB0\xAC\xEA\xA0\xBE\xA2\x06\x0D\x76\x35\x7E\x76\x1B\x32\x18\x66"+0x45D61C0D).s,/*http://bbs.300yx.net/*/XorStr<0xDE,22,0xB3CB6DB7>("\xB6\xAB\x94\x91\xD8\xCC\xCB\x87\x84\x94\xC6\xDA\xDA\xDB\x95\x95\xC0\x81\x95\x85\xDD"+0xB3CB6DB7).s,MB_OK);
+		return;
+	}
+
 	LoadDataFile();
 
 	*(DWORD*)&pSendDestroyPacket = 0x00520130;
@@ -492,7 +548,7 @@ void Initialize()
 
 	//0093A8E0
 
-	LoadLibraryA("300camera.dll");
+	LoadLibraryA(/*300camera.dll*/XorStr<0x25,14,0xA16368AB>("\x16\x16\x17\x4B\x48\x47\x4E\x5E\x4C\x00\x4B\x5C\x5D"+0xA16368AB).s);
 	bIsHooked = TRUE;
 
 	SetTimer(NULL, 1000, 1, timerProc);
