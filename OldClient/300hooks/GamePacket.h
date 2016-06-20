ced7592ea4bc134ae64342d1386ff94ba12c2194
number201724 <number201724@me.com>
2016-06-21 01:21:47 +0800

    include 300hooks

---
+++ OldClient/300hooks/GamePacket.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#pragma pack(1)
+struct net_packet_s
+{
+	unsigned long length;
+	unsigned long timestamp;
+	unsigned short identifier;
+};
+
+typedef net_packet_s net_packet_t;
+#pragma pack()
+class GamePacket
+{
+public:
+	GamePacket(void);
+	~GamePacket(void);
+
+	HHOOK m_pPrevHook;
+	void* m_pGetPlayerMngr;
+	void* m_pGetNetInstance;
+	void* m_pPreRecvMessage;
+	void* m_pPreSendMessage;
+	
+
+	DECLARE_INSTANCE(GamePacket);
+
+	void* FindPlayerMngr();
+
+	static void __stdcall PreRecvMessage(net_packet_t* packet);
+	static void __stdcall PreSendMessage(net_packet_t* packet);
+	static LRESULT CALLBACK HOOK_WndProc(int code, WPARAM wParam, LPARAM lParam);
+
+	void SwapMessage(net_packet_t* packet,BOOL IsSend);
+
+	void Attach();
+	void Detach();
+};
+
